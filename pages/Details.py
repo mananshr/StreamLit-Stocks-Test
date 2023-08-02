8ef6d7e73f715fd03b114272298f639822599787
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-08-02 13:40:41 +0530

    Icrawler, solved

--- pages/Details.py
+++ pages/Details.py
@@ -1,9 +1,9 @@
+import os
+
 import streamlit as st
 import yfinance
+from PIL import Image
 from icrawler.builtin import GoogleImageCrawler
-from icrawler import ImageDownloader
-
-import os
 
 symbol = st.selectbox(
     "Select a stock to analyse",
@@ -54,15 +54,38 @@ f"# {longName}"
 companyOfficers = info.get("companyOfficers")
 companyOfficerName = companyOfficers[0].get("name")
 
-class MyImageDownloader(ImageDownloader):
-    def get_filename(self, task, default_ext):
-        filename = super(MyImageDownloader, self).get_filename(
-            task, default_ext)
-        print(filename)
-        return '{}_{}'.format(filename, default_ext)
+# class MyImageDownloader(ImageDownloader):
+#     def get_filename(self, task, default_ext):
+#         url_path = urlparse(task['file_url'])[2]
+#         # print("Person: "+companyOfficerName)
+#         print("URL: "+url_path)
+#         if '.' in url_path:
+#             extension = url_path.split('.')[-1]
+#             if extension.lower() not in [
+#                 'jpg', 'jpeg', 'png', 'bmp', 'tiff', 'gif', 'ppm', 'pgm'
+#             ]:
+#                 extension = default_ext
+#         else:
+#             extension = default_ext
+#         # works for python3
+#         filename = base64.b64encode(url_path.encode()).decode()
+#         print("File: "+filename)
+#         return '{}.{}'.format(filename, extension)
 
-google_crawler = GoogleImageCrawler(feeder_threads=1, parser_threads=1, downloader_threads=1, storage={'root_dir': 'images\\'+symbol}, downloader_cls=MyImageDownloader)
+# google_crawler = GoogleImageCrawler(feeder_threads=1, parser_threads=1, downloader_threads=1, storage={'root_dir': 'images\\'+symbol}, downloader_cls=MyImageDownloader)
+google_crawler = GoogleImageCrawler(feeder_threads=1, parser_threads=1, downloader_threads=1, storage={'root_dir': 'images\\'+symbol})
 google_crawler.crawl(keyword=companyOfficerName, max_num=1)
+
+path = os.getcwd().removesuffix("pages")
+path = 'D:\Code\PY\StreamlitTest2\pages\\'
+# image_path = os.getcwd()+"\\images\\"+symbol+"\\000001.jpg"
+image_path = path+"images\\"+symbol
+image_path = image_path + "\\" + os.listdir(image_path)[0]
+# print("")
+print("PATH: "+image_path)
+# image_path = image_path.replace("\\\\","\\")
+image = Image.open(image_path)
+st.image(image, companyOfficerName)
 
 # directory = os.getcwd()
 # print(directory)
